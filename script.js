3b819d3b4f04bd6ca8ce8971f3784f9c6e930982
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-08 00:51:37 -0500

    Update to ATM screen

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Declaration: e.g let screen -- Creates a space in memory called 'Screen'
+// Assigment: Data put in to the space created by the declaration e.g " WELCOME TO DREAM BIG BANK" is placed/stored in that space in memory called 'Screen'
+
+const title = document.querySelector('.title')
+console.log(title)
+// title.innerText = 'gah' === telling the title to change it's inner text to the string gah
+
+const amount = document.querySelector('.amount')
+console.log(amount)
+
+// LeftButtons
+const withdrawalButton = document.querySelector('.withdrawal')
+const depositButton = document.querySelector('.deposit')
+const resetButton = document.querySelector('.reset')
+
+// RightButtons
+const balanceButton = document.querySelector('.balance')
+const fastCashButton = document.querySelector('.fastCash')
+const transferButton = document.querySelector('.transfer')
+
+//Key Pad
+
+
+
+
+
+
 
